17672fd6cb876c3f2506fca8be7b507ba725a506
Raunik47 <rauniksingh787878@gmail.com>
2025-05-15 20:20:11 +0530

    index.js logic done

---
+++ index.js
@@ -0,0 +1,62 @@
+const express=require("express");
+const app=express();
+
+const userRoutes=require("./routes/User");
+const profileRoutes=require("./routes/Profile");
+const paymentRoutes=require("./routes/Payments");
+const courseRoutes=require("./routes/Course");
+
+const database=require("./config/database")
+const cookieParser=require("cookie-parser");
+const cors=require("cors");
+const fileUpload=require("express-fileupload");
+const dotenv=require("dotenv"); 
+dotenv.config();
+const {cloudinaryConnect}=require("./config/cloudinary");
+
+const PORT= process.env.PORT || 4000;
+
+// database connect
+
+database.connect();
+
+// middleware
+app.use(express.json());
+app.use(cookieParser);
+// this midleware is used to active the backend for frontend request
+app.use(
+    cors({
+        origin:"http://localhost:3000",
+        credentials:true,
+    })
+)
+app.use(
+    fileUpload({
+        useTempFiles:true,
+        tempFileDir:"/tmp"
+
+    })
+)
+// cloudinary connection
+cloudinaryConnect();
+// routes
+app.use("/api/v1/auth",userRoutes);
+app.use("/api/v1/profile",profileRoutes);
+app.use("/api/vi/course",courseRoutes);
+app.use("/api/v1/payments",paymentRoutes);
+
+// def route
+app.get("/",(req,res) =>{
+    return res.status(200).json({
+        success:true,
+        message:"your server is up and running..."
+    });
+})
+app.listen(PORT,()=>{
+  console.log(  `App is running at ${PORT}`)  
+})
+
+
+
+
+
